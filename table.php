88f56644369e32b4009fc875bf0a79c9205b7541
Igor G <io.gaev@gmail.com>
2018-03-04 22:20:41 +0500

    Добавлены стили таблице

--- table.php
+++ table.php
@@ -1,7 +1,6 @@
 <?php
 $data_json = file_get_contents(__DIR__.'/adress_base.json');
 $data = json_decode($data_json, true);
-
 ?>
 
 <!doctype html>
@@ -12,24 +11,10 @@ $data = json_decode($data_json, true);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Таблица адресов и телефонов</title>
-    <style type="text/css">
-        TABLE {
-            width: 300px;
-            border-collapse: collapse;
-        }
-        TD {
-            padding: 3px;
-            border: 1px solid black;
-        }
-        thead {
-            background: #b0e0e6;
-        }
-    </style>
+    <style type="text/css"> TABLE {width: 300px; border-collapse: collapse;} TD {padding: 3px;border: 1px solid black; } thead {font-weight: bold; background: #b0e0e6;}</style>
 </head>
 <body>
-<pre>
-    <?php print_r($data) ?>
-</pre>
+<h1>Информация о пользователях</h1>
   <table>
       <thead>
       <tr>
@@ -60,6 +45,5 @@ $data = json_decode($data_json, true);
         <?php endforeach;?>
       </tbody>
   </table>
-
 </body>
 </html>
